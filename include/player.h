38b60b8979020546824e486fa6f31afa8679bf6c
DiogoCunhaaa <diogo_f_cunha@estudante.sesisenai.org.br>
2025-07-12 20:52:02 -0300

    Update: refactored the player.c code

--- include/player.h
+++ include/player.h
@@ -3,22 +3,35 @@
 
 #include "raylib.h"
 
-//Structure that represents the player 
-typedef struct Player {
+typedef enum
+{
+    PLAYER_IDLE,
+    PLAYER_WALK,
+    PLAYER_JUMP,
+    PLAYER_ATTACK,
+    PLAYER_STATE_COUNT, // Always last
+} PlayerState;
+
+// Structure that represents the player
+typedef struct Player
+{
     Vector2 position;
     Vector2 velocity;
     float width;
     float height;
     bool isOnGround;
+    PlayerState state;
     int frame;
     float frameTime;
-    float frameSpeed;
-    int maxFrames;
+    float frameSpeed; // Velocity the sprite updates
+    int maxFrames; // Frames the sprite has
+    float actionTimer;// In the future, to set timer to actions like attack or dashes
+
 } Player;
 
-//Player related functions 
-void InitPlayer(Player *player); //iniciates the player 
-void UpdatePlayer(Player *player, float delta, Rectangle *platforms, int platformCount); //update the player 
-void DrawPlayer(Player player, Texture2D texture); // draw the player on screen
+// Player related functions
+void InitPlayer(Player *player);                                                         // iniciates the player
+void UpdatePlayer(Player *player, float delta, Rectangle *platforms, int platformCount); // update the player
+void DrawPlayer(Player player, Texture2D texture);                                       // draw the player on screen
 
 #endif
--- src/player.c
+++ src/player.c
@@ -1,6 +1,6 @@
 /*-------------------------------------
-* I know it should be player-> instead of (*player)
-* but i started this way and its too much work to do if i wanted to refactor the entire code
+* player.c
+* HEADLINE
 ---------------------------------------*/
 
 #include "player.h"
@@ -13,140 +13,150 @@
 #define WORLD_WIDTH 1950
 #define WORLD_HEIGHT 450
 
+// Internal functions, use restrictly by the player.c
+static void HandleMovementInput(Player *player);
+static void ApplyPhysics(Player *player, float delta, Rectangle *platforms, int platformCount);
+static void UpdateState(Player *player, float delta);
+static void UpdateAnimation(Player *player, float delta);
+
 void InitPlayer(Player *player)
 {
-    (*player).position = (Vector2){100, 300};
-    (*player).velocity = (Vector2){0, 0};
-    (*player).width = 20;
-    (*player).height = 40;
-    (*player).isOnGround = false;
-    (*player).frame = 0;
-    (*player).frameTime = 0.0f;
-    (*player).frameSpeed = 6.0f; // Frames per second
-    (*player).maxFrames = 8;     // Total number of frames
+    player->position = (Vector2){100, 300};
+    player->velocity = (Vector2){0, 0};
+
+    player->width = 20;
+    player->height = 40;
+
+    player->isOnGround = false;
+    player->state = PLAYER_IDLE;
+
+    player->frame = 0;
+    player->frameTime = 0.0f;
+    player->frameSpeed = 4.0f; // default: idle
+    player->maxFrames = 8;     // default: idle
+
+    player->actionTimer = 0.0f;
+}
+
+// internal functions
+static void HandleMovementInput(Player *player)
+{
+    if (player->state != PLAYER_ATTACK)
+    {
+        if (IsKeyDown(KEY_A))
+        {
+            player->velocity.x = -MOVE_SPEED; // make the player walk left by making his speed -200.0f
+        }
+        else if (IsKeyDown(KEY_D))
+        {
+            player->velocity.x = MOVE_SPEED; // make the player walk right by making his speed 200.0f
+        }
+        else
+        {
+            player->velocity.x = 0; // makes sure the player stays stopped
+        }
+
+        if (player->isOnGround && IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_W))
+        {
+            player->velocity.y = JUMP_FORCE; // makes the player jump
+            player->isOnGround = false;      // makes sure the player can only jump when on ground
+        }
+    }
+}
+
+static void ApplyPhysics(Player *player, float delta, Rectangle *platforms, int platformCount)
+{
+    bool onGround = false;
+    player->velocity.y += GRAVITY * delta;
+
+    Rectangle playerRect = {
+        player->position.x,
+        player->position.y,
+        player->width,
+        player->height,
+    };
+
+    // Horizontal movement and collision
+    player->position.x += player->velocity.x * delta;
+    playerRect.x = player->position.x;
+
+    for (int i = 0; i < platformCount; i++)
+    {
+        Rectangle plat = platforms[i];
+
+        if (CheckCollisionRecs(playerRect, plat))
+        {
+            if (player->velocity.x > 0)
+            {
+                player->position.x = plat.x - player->width;
+            }
+            else if (player->velocity.x < 0)
+            {
+                player->position.x = plat.x + plat.width;
+            }
+
+            player->velocity.x = 0;
+            playerRect.x = player->position.x;
+        }
+    }
+
+    // Vertical movement and collision
+    player->position.y += player->velocity.y * delta;
+    playerRect.y = player->position.y;
+
+    for (int i = 0; i < platformCount; i++)
+    {
+        Rectangle plat = platforms[i];
+
+        if (CheckCollisionRecs(playerRect, plat))
+        {
+            float playerTop = player->position.y;
+            float playerBottom = player->position.y + player->height;
+            float platTop = plat.y;
+            float platBottom = plat.y + plat.height;
+
+            if (player->velocity.y > 0 && playerBottom > platTop)
+            {
+                player->position.y = plat.y - player->height;
+                player->velocity.y = 0;
+                onGround = true;
+            }
+            else if (player->velocity.y < 0 && playerTop < platBottom)
+            {
+                player->position.y = platBottom;
+                player->velocity.y = 0;
+            }
+
+            playerRect.y = player->position.y;
+        }
+    }
+
+    player->isOnGround = onGround;
+
+    // Clamp to avoid the player from exiting the screen
+    player->position.x = MyClamp(player->position.x, 0, WORLD_WIDTH - player->width);
+    player->position.y = MyClamp(player->position.y, 0, WORLD_HEIGHT - player->height);
+}
+
+static void UpdateAnimation(Player *player, float delta)
+{
+    if (player->frameSpeed <= 0)
+        return;
+
+    player->frameTime += delta;
+    if (player->frameTime >= 1.0f / player->frameSpeed)
+    {
+        player->frameTime = 0;
+        player->frame = (player->frame + 1) % player->maxFrames;
+    }
 }
 
 // Update
 void UpdatePlayer(Player *player, float delta, Rectangle *platforms, int platformCount)
 {
-    bool onGround = false;
-
-    (*player).velocity.y += GRAVITY * delta;
-
-    if (IsKeyDown(KEY_A))
-    {
-        (*player).velocity.x = -MOVE_SPEED; // make the player walk left by making his speed -200.0f
-    }
-    else if (IsKeyDown(KEY_D))
-    {
-        (*player).velocity.x = MOVE_SPEED; // make the player walk right by making his speed 200.0f
-    }
-    else
-    {
-        (*player).velocity.x = 0; // makes sure the player stays stopped
-    }
-
-    if ((*player).isOnGround && IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_W)) // CHANGE TO if ((*player).isOnGround && (IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_W))) AFTER TESTS
-    {
-        (*player).velocity.y = JUMP_FORCE; // makes the player jump
-        (*player).isOnGround = false;      // makes sure the player can only jump when on ground
-    }
-
-    Rectangle playerRect = {
-        (*player).position.x,
-        (*player).position.y,
-        (*player).width,
-        (*player).height,
-    };
-
-    // moves the player based on his actual position
-    // On the x axys
-    (*player).position.x += (*player).velocity.x * delta; // Moves horizontaly
-
-    // HORIZONTAL MOVEMENT + LATERAL COLLISION
-    playerRect.x = (*player).position.x;
-    playerRect.y = (*player).position.y;
-
-    for (int i = 0; i < platformCount; i++)
-    {
-        Rectangle plat = platforms[i];
-
-        if (CheckCollisionRecs(playerRect, plat))
-        {
-            if ((*player).velocity.x > 0)
-            {
-                (*player).position.x = plat.x - (*player).width;
-            }
-            else if ((*player).velocity.x < 0)
-            {
-                (*player).position.x = plat.x + plat.width;
-            }
-
-            (*player).velocity.x = 0;
-
-            // Updates the rect
-            playerRect.x = (*player).position.x;
-        }
-    };
-
-    // VERTICAL MOVEMENT + VERTICAL COLLISION
-    // Moves the player on his y axys
-    (*player).position.y += (*player).velocity.y * delta;
-
-    playerRect.y = (*player).position.y;
-
-    onGround = false;
-
-    for (int i = 0; i < platformCount; i++)
-    {
-        Rectangle plat = platforms[i];
-
-        if (CheckCollisionRecs(playerRect, plat))
-        {
-            // Collision variables
-            float playerTop = (*player).position.y;
-            float playerBottom = (*player).position.y + (*player).height;
-            float platTop = plat.y;
-            float platBottom = plat.y + plat.height;
-
-            if ((*player).velocity.y > 0 && playerBottom > platTop)
-            {
-                // Feet collision with the platform
-                (*player).position.y = plat.y - (*player).height;
-                (*player).velocity.y = 0;
-                onGround = true;
-            }
-            else if ((*player).velocity.y < 0 && playerTop < platBottom)
-            {
-                // Head collision with the platform
-                (*player).position.y = platBottom;
-                (*player).velocity.y = 0;
-            }
-
-            // Updates the rect
-            playerRect.y = (*player).position.y;
-        }
-    }
-
-    (*player).isOnGround = onGround;
-
-    // Clamp to avoid the player from exiting the screen
-    (*player).position.x = MyClamp((*player).position.x, 0, WORLD_WIDTH - (*player).width);
-    (*player).position.y = MyClamp((*player).position.y, 0, WORLD_HEIGHT - (*player).height);
-
-    (*player).frameTime += delta;
-    if ((*player).velocity.x != 0)
-    {
-        if ((*player).frameTime >= 1.0f / (*player).frameSpeed)
-        {
-            (*player).frameTime = 0;
-            (*player).frame = ((*player).frame + 1) % (*player).maxFrames;
-        }
-    }
-    else
-    {
-        (*player).frame = 0;
-    }
+    HandleMovementInput(player);
+    ApplyPhysics(player, delta, platforms, platformCount);
+    UpdateAnimation(player, delta);
 };
 
 // Draw
